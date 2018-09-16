daece88d0f71191d50bfa325bce7cfe695aa7c28
Felipe Castillo <fcastillo.ec@gmail.com>
2018-09-16 14:04:14 -0700

    Add Travis script and deploy script

---
+++ bin/removeNotTranslated.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const wrapperPath = path.join(__dirname, '../locales', 'wrapper');
+const corePath = path.join(__dirname, '../locales', 'core');
+
+const wrapperFilenames = fs.readdirSync(wrapperPath);
+const coreFilenames = fs.readdirSync(corePath);
+
+// Removes all keys that have NOT_TRANSLATED values
+function notTranslated(key, value) {
+  if (value === '__NOT_TRANSLATED__') {
+    return undefined;
+  }
+  return value;
+}
+
+// Rewrites JSON language files for wrapper
+wrapperFilenames.forEach((file) => {
+  const filePath = path.join(wrapperPath, file);
+  const json = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  fs.writeFileSync(filePath, `${JSON.stringify(json, notTranslated, 2)}\n`);
+});
+
+// Rewrites JSON language files for core
+coreFilenames.forEach((file) => {
+  const filePath = path.join(corePath, file);
+  const json = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  fs.writeFileSync(filePath, `${JSON.stringify(json, notTranslated, 2)}\n`);
+});
